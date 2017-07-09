9b1861667a188843ea2c1ebff16fdcaaabbf80fc
OJW <OJW@1.233.83.210>
2017-07-10 01:27:07 +0900

    벌처!

--- src/pre/combat/VultureTravelManager.java
+++ src/pre/combat/VultureTravelManager.java
@@ -8,11 +8,9 @@ import java.util.Map;
 import bwta.BaseLocation;
 import pre.main.MyBotModule;
 import pre.manager.InformationManager;
+import pre.util.MicroSet;
 
 public class VultureTravelManager {
-
-	private static final int ASSIGNED_FRAME = 24 * 20; // 20초
-	private static final int VISIT_FRAME = 24 * 30; // 30초
 
 	private Map<String, TravelSite> squadSiteMap = new HashMap<>();
 	
@@ -47,18 +45,20 @@ public class VultureTravelManager {
 	public void update() {
 		if (!initialized) {
 			init();
+			return;
 		}
 		
 		for (TravelSite travelSite : travelSites) {
 			if (MyBotModule.Broodwar.isVisible(travelSite.baseLocation.getTilePosition())) {
+				// 1. 시야가 밝혀졌다면 visitFrame을 계속 업데이트 한다.
 				travelSite.visitFrame = MyBotModule.Broodwar.getFrameCount();
 				
-				// 해당 travelSite를 방문예정인 스쿼드의 travelSite를 변경한다.
+				// 2. 시야가 밝혀진 travelSite를 방문예정인 스쿼드의 travelSite를 변경한다.
 				String relatedSquadName = null;
 				for (String squadName : squadSiteMap.keySet()) {
 					TravelSite site = squadSiteMap.get(squadName);
-					if (site.baseLocation.equals(travelSite.baseLocation)) {
-						relatedSquadName = squadName;
+					if (site.baseLocation.getPosition().equals(travelSite.baseLocation.getPosition())) {
+						relatedSquadName = squadName; // 방문에정 스쿼드 명
 						break;
 					}
 				}
@@ -67,17 +67,20 @@ public class VultureTravelManager {
 					System.out.println("change travel site");
 					BaseLocation currentBase = squadSiteMap.get(relatedSquadName).baseLocation;
 					squadSiteMap.remove(relatedSquadName);
-					getBestTravelSite(relatedSquadName, currentBase);
+					getBestTravelSite(relatedSquadName, currentBase); // travelSite 변경
 				}
 			}
 		}
 	}
-	
+
+	// 벌처스쿼드가 정찰할 base를 선택한다.
+	// 1. 방문한지 가장 오래된 location으로 이동
+	// 2. 방문한지 가장 오래된 장소가 복수개이면(아예 정찰이 안된) 할당된지 가장 오래된 location으로 이동
+	// 3. 방문 및 할당된지 동일하게 오래된 장소가 복수개이면, 가까운 곳부터 간다. (currentBase에서 가장 가까운 곳, squadName만 입력받는 경우 적 앞마당에서 가장 가까운 곳)
 	public BaseLocation getBestTravelSite(String squadName) {
 		BaseLocation firstExpansion = InformationManager.Instance().getFirstExpansionLocation(InformationManager.Instance().enemyPlayer);
 		return getBestTravelSite(squadName, firstExpansion);
 	}
-	
 	
 	public BaseLocation getBestTravelSite(String squadName, BaseLocation currentBase) {
 		if (!initialized) {
@@ -89,8 +92,8 @@ public class VultureTravelManager {
 			return site.baseLocation;
 		}
 		
-		int longestVisitPassedFrame = VISIT_FRAME;
-		int longestAssignedPassedFrame = ASSIGNED_FRAME;
+		int longestVisitPassedFrame = MicroSet.Vulture.visitFrame;
+		int longestAssignedPassedFrame = MicroSet.Vulture.assignedFrame;
 		double shortestDistance = 999999.0;
 		
 		TravelSite bestTravelSite = null;
@@ -98,7 +101,6 @@ public class VultureTravelManager {
 		int currentFrame = MyBotModule.Broodwar.getFrameCount();
 		for (TravelSite travelSite : travelSites) {
 			if (squadSiteMap.values().contains(travelSite)) {
-				System.out.println("dulplicated");
 				continue;
 			}
 			
