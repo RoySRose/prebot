ee989fd5db6361c9984cd655c741a33f69649618
staeminba <staeminba@gamil.com>
2018-06-16 18:47:13 +0900

    [180616] 마이크로 베슬 추가

--- src/main/java/prebot/micro/control/airforce/VesselControl.java
+++ src/main/java/prebot/micro/control/airforce/VesselControl.java
@@ -3,16 +3,242 @@ package prebot.micro.control.airforce;
 import java.util.List;
 
 import bwapi.Position;
+import bwapi.TechType;
 import bwapi.Unit;
+import bwapi.UnitType;
+import bwapi.WeaponType;
+import prebot.common.MapGrid;
+import prebot.common.constant.CommonCode.PlayerRange;
+import prebot.common.main.Prebot;
+import prebot.common.util.CommandUtils;
+import prebot.common.util.MicroUtils;
+import prebot.common.util.UnitUtils;
+import prebot.micro.DecisionMaker;
+import prebot.micro.FleeOption;
+import prebot.micro.KitingOption;
+import prebot.micro.TargetScoreCalculators;
+import prebot.micro.constant.MicroConfig;
+import prebot.micro.constant.MicroConfig.Angles;
 import prebot.micro.control.Control;
+import prebot.strategy.StrategyIdea;
+import prebot.strategy.StrategyManager;
 import prebot.strategy.UnitInfo;
+import prebot.strategy.constant.StrategyConfig.EnemyStrategy;
 
 public class VesselControl extends Control {
+
+	private final static double sVesselCheckSpeed = UnitType.Terran_Science_Vessel.topSpeed() * 8;
+	private final static int sVesselCheckRadius = UnitType.Terran_Science_Vessel.sightRange();
 
 	@Override
 	public void control(List<Unit> unitList, List<UnitInfo> euiList, Position targetPosition) {
 		// TODO Auto-generated method stub
+
+		FleeOption fOption = new FleeOption(targetPosition, false, Angles.WIDE);
+		KitingOption kOption = new KitingOption(fOption, false);
+
+		Position orderPosition = targetPosition;
+
+		for (Unit vessel : unitList) {
+			/*
+			 * 오리더 체크 - 렉 체크 하는것 같아서 일단 주석 if
+			 * (!TimeUtils.executeUnitRotation(vessel, LagObserver.groupsize()))
+			 * { continue; }
+			 */
+			if (StrategyManager.Instance().getCurrentStrategyBasic() == EnemyStrategy.ZERGBASIC_MUTALMANY) {
+
+				// 싸베 이리디 쓰기
+				if (vessel.getEnergy() >= 75) {
+					List<Unit> matrix_targets = null;
+					matrix_targets = UnitUtils.getUnitsInRadius(PlayerRange.ENEMY, vessel.getPosition(), sVesselCheckRadius + 300,UnitType.Zerg_Mutalisk); 
+					for (Unit target : matrix_targets) {
+						if (target.getHitPoints() > target.getType().maxHitPoints() * 0.9) {
+							CommandUtils.useTechTarget(vessel, TechType.Irradiate, target);
+							return;
+						}
+					}
+				}
+			}
+			// 싸베 디펜시브 매트릭스 쓰기
+			else if (vessel.getEnergy() >= 100) {
+				List<Unit> matrix_targets = null;
+					matrix_targets = UnitUtils.getUnitsInRadius(PlayerRange.SELF, vessel.getPosition(), sVesselCheckRadius + 300); 
+				for (Unit target : matrix_targets) {
+					if (target.getType() == UnitType.Terran_SCV	|| target.getType() == UnitType.Terran_Vulture_Spider_Mine) {
+						continue;
+					}
+					if (target.isUnderAttack() && target.getHitPoints() < target.getType().maxHitPoints() * 0.7
+							&& !target.isDefenseMatrixed()
+							&& target.getHitPoints() > target.getType().maxHitPoints() * 0.15) {
+						CommandUtils.useTechTarget(vessel, TechType.Defensive_Matrix, target);
+						return;
+					}
+				}
+			}
+
+			Unit invisibleEnemyUnit = null;
+			int closestDistToVessel = 100000;
+			for (UnitInfo unitInfo : euiList) {
+				Unit unit = unitInfo.getUnit();
+				if (unit.isVisible() && unit.isDetected() && unit.getPosition().isValid()) {
+					if (unit.getType() == UnitType.Protoss_Dark_Templar || unit.getType() == UnitType.Zerg_Lurker
+							|| unit.getType() == UnitType.Terran_Wraith || unit.getType() == UnitType.Terran_Ghost
+							|| unit.getType() == UnitType.Protoss_Arbiter) {
+						int tempdist = unit.getDistance(vessel);
+
+						if (tempdist < closestDistToVessel) {
+							invisibleEnemyUnit = unit;
+							closestDistToVessel = tempdist;
+						}
+					}
+				}
+			}
+
+			if (invisibleEnemyUnit == null) {
+				closestDistToVessel = 100000;
+				for (UnitInfo unitInfo : euiList) {
+					Unit unit = unitInfo.getUnit();
+					if (unit.isVisible() && (!unit.isDetected() || unit.getOrder() == unit.getOrder().Burrowing)
+							&& unit.getPosition().isValid()) {
+						int tempdist = unit.getDistance(vessel);
+						if (tempdist < closestDistToVessel) {
+							invisibleEnemyUnit = unit;
+							closestDistToVessel = tempdist;
+						}
+					}
+				}
+			}
+
+			if (invisibleEnemyUnit != null) {
+				List<Unit> nearallies = UnitUtils.getUnitsInRadius(PlayerRange.ENEMY, vessel.getPosition(), UnitType.Terran_Science_Vessel.sightRange());
+				if (nearallies.size() > 2) {
+					orderPosition = invisibleEnemyUnit.getPosition();// 움직이시오.
+					if (invisibleEnemyUnit.getDistance(vessel) < UnitType.Terran_Science_Vessel.sightRange() * 2 / 3) {
+						orderPosition = vessel.getPosition();
+					}
+				}
+			}
+
+			Unit mostDangerousTarget = null;
+			double mostDangercheck = -99999;
+
+			List<Unit> dangerous_targets = null;
+			dangerous_targets = UnitUtils.getUnitsInRadius(PlayerRange.ENEMY, vessel.getPosition(), sVesselCheckRadius); 
+			for (Unit target : dangerous_targets) {
+
+				double temp = target.getType().airWeapon().maxRange()
+						- target.getPosition().getDistance(vessel.getPosition());
+				if (temp > mostDangercheck) {
+					mostDangerousTarget = target;
+					mostDangercheck = temp;
+				}
+			}
+
+
+			boolean fleeing = false;
+			if (mostDangerousTarget != null) {
+				double temp = 0;
+				if (!mostDangerousTarget.getType().isBuilding()) {
+					temp = sVesselCheckSpeed * 3;
+				}
+				if (mostDangerousTarget.isInWeaponRange(vessel)
+						|| (vessel.getDistance(mostDangerousTarget) <= Prebot.Broodwar.enemy().weaponMaxRange(
+								mostDangerousTarget.getType().airWeapon()) + sVesselCheckSpeed + temp)) {
+					Position fleePosition = getFleePosition(vessel, mostDangerousTarget.getPosition(), (int) sVesselCheckSpeed,fOption);
+					vessel.move(fleePosition);
+					fleeing = true;
+				}
+			}
+
+			if (!fleeing) {
+				CommandUtils.move(vessel, orderPosition);
+			}
+		}
+	}
+	
+	public static Position getFleePosition(Unit rangedUnit, Position target,int moveDistPerSec, FleeOption fleeOption) {
+		int reverseX = rangedUnit.getPosition().getX() - target.getX(); // 타겟과 반대로 가는 x양
+		int reverseY = rangedUnit.getPosition().getY() - target.getY(); // 타겟과 반대로 가는 y양
+	    final double fleeRadian = Math.atan2(reverseY, reverseX); // 회피 각도
+	    
+		Position safePosition = null;
+		double minimumRisk = 99999;
+		int minimumDistanceToGoal = 99999;
+
+		Integer[] FLEE_ANGLE = (Integer[]) (fleeOption.angles != null ? fleeOption.angles : MicroConfig.FleeAngle.getFleeAngle(rangedUnit.getType()));
+		double fleeRadianAdjust = fleeRadian; // 회피 각(radian)
+		int moveCalcSize = moveDistPerSec; // 이동 회피지점의 거리 = 유닛의 초당이동거리
 		
+		while (safePosition == null && moveCalcSize < 1200) {//TODO 이거 회피지역 안나오면 무한 Loop 아닐까??? moveCalcsize 때문에 무한은 안 돌테지만... 사실상 moveCalcsize 는 늘어나야 찾을수 있을텐데
+			for(int i = 0 ; i< FLEE_ANGLE.length; i ++) {
+				
+			    Position fleeVector = new Position((int)(moveCalcSize * Math.cos(fleeRadianAdjust)), (int)(moveCalcSize * Math.sin(fleeRadianAdjust))); // 이동벡터
+				Position movePosition = new Position(rangedUnit.getPosition().getX() + fleeVector.getX(), rangedUnit.getPosition().getY() + fleeVector.getY()); // 회피지점
+				//Position middlePosition = new Position(rangedUnit.getPosition().getX() + fleeVector.getX() / 2, rangedUnit.getPosition().getY() + fleeVector.getY() / 2); // 회피중간지점
+				
+				if(rangedUnit.getID() == 102){
+				//System.out.print("fleeVector: " + fleeVector.toString());
+				//System.out.print("movePosition: " + movePosition.toString());
+				//System.out.print("middlePosition: " + middlePosition.toString());
+				}
+				
+				double risk = 0;
+				risk = riskOfFleePositionAir(rangedUnit, movePosition, moveCalcSize, fleeOption.united); // 회피지점에서의 예상위험도
+				
+				int distanceToGoal = movePosition.getApproxDistance(fleeOption.goalPosition); // 위험도가 같을 경우 2번째 고려사항: 목표지점까지의 거리
+
+				if (risk < 100 	&& (risk < minimumRisk || ((risk == minimumRisk) && distanceToGoal < minimumDistanceToGoal))) {
+				
+					//System.out.print("selected: " + i + "  ");
+					safePosition =  movePosition;
+					//System.out.println("setting position : " +safePosition.getX() + ","+safePosition.getY());
+					minimumRisk = risk;
+					minimumDistanceToGoal = distanceToGoal;
+				}
+				fleeRadianAdjust = MicroUtils.rotate(fleeRadian, FLEE_ANGLE[i]); // 각도변경
+		    }
+			if (safePosition == null) { // 회피지역이 없을 경우 1) 회피거리 짧게 잡고 다시 조회 
+		    	moveCalcSize = moveCalcSize * 2; //TODO 보정 로직이 moveCalcsize 늘려야 하는거 같은데..또한 늘렸을시에 무한 loop 조심
+			}
+		}
+		if (safePosition == null) { // 회피지역이 없을 경우 3) 목표지점으로 간다. 이 경우는 거의 없다.
+			safePosition = fleeOption.goalPosition;
+		}
+		
+	    return safePosition;
+	}
+	
+	
+	/**
+	 * 회피지점의 위험도
+	 * 
+	 * @param unitType
+	 * @param position
+	 * @param radius
+	 * @param united
+	 * @return
+	 */
+
+	public static double riskOfFleePositionAir(Unit myunit, Position position, int radius, boolean united) {
+		double risk = 0;
+		
+		List<Unit> dangerous_targets = null;
+		dangerous_targets = UnitUtils.getUnitsInRadius(PlayerRange.ENEMY, position, sVesselCheckRadius, UnitType.Terran_Missile_Turret); 
+		dangerous_targets.addAll(UnitUtils.getUnitsInRadius(PlayerRange.ENEMY, position, sVesselCheckRadius, UnitType.Terran_Goliath));
+			for (Unit enemyunits : dangerous_targets) {
+				
+				double inrange =  Prebot.Broodwar.enemy().weaponMaxRange(enemyunits.getType().airWeapon());
+				double additionalrange = sVesselCheckSpeed;		
+				double distance = enemyunits.getDistance(position);
+				if(enemyunits.getType().airWeapon() != WeaponType.None){
+					if(distance<inrange){
+						risk += 100;
+					}else{
+						risk = (additionalrange - (distance - inrange))/additionalrange * 100;
+					}
+				}
+			}
+		return risk;
 	}
 
 }
