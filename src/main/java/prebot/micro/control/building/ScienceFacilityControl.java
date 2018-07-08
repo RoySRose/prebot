f2cc2341c1821b58424d9eb4a81936bf7b153745
Roy Kim <sungwook0115.kim@gmail.com>
2018-07-08 23:17:05 +0900

    fly logic for building

--- src/main/java/prebot/micro/control/building/ScienceFacilityControl.java
+++ src/main/java/prebot/micro/control/building/ScienceFacilityControl.java
@@ -4,15 +4,24 @@ import java.util.List;
 
 import bwapi.Position;
 import bwapi.Unit;
+import prebot.micro.control.BuildingFlyControl;
 import prebot.micro.control.Control;
 import prebot.strategy.UnitInfo;
 
-public class ScienceFacilityControl extends Control {
+public class ScienceFacilityControl extends BuildingFlyControl {
+
+    public ScienceFacilityControl() {
+        super(false, false);
+    }
 
 	@Override
 	public void control(List<Unit> unitList, List<UnitInfo> euiList) {
 		// TODO Auto-generated method stub
-		
+        executeFly(unitList, euiList);
 	}
 
+    @Override
+    public void checkFlyCondition() {
+
+    }
 }
