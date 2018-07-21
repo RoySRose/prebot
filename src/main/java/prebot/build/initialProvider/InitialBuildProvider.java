d622e1a930553541c6a8fb70c198dbb9a495aae4
yahokuku <yahokuku@naver.com>
2018-07-21 19:48:35 +0900

    20180721. hkk  - 이니셜 빌드전 마린 생산 로직 추가

--- src/main/java/prebot/build/initialProvider/InitialBuildProvider.java
+++ src/main/java/prebot/build/initialProvider/InitialBuildProvider.java
@@ -11,8 +11,13 @@ import prebot.build.initialProvider.buildSets.VsZerg;
 import prebot.build.prebot1.BuildManager;
 import prebot.build.prebot1.BuildOrderItem;
 import prebot.build.prebot1.BuildOrderQueue;
+import prebot.build.prebot1.ConstructionManager;
+import prebot.common.main.Prebot;
 import prebot.common.util.FileUtils;
 import prebot.strategy.InformationManager;
+import prebot.strategy.StrategyIdea;
+import prebot.strategy.constant.EnemyStrategy;
+import prebot.strategy.constant.EnemyStrategyOptions;
 
 /// 봇 프로그램 설정
 public class InitialBuildProvider {
@@ -22,6 +27,10 @@ public class InitialBuildProvider {
 	public static InitialBuildProvider Instance() {
 		return instance;
 	}
+	
+	public int nowMarine = 0;
+	
+	public int orderMarine  = 1;
 
 	public boolean InitialBuildFinished = false;
 	
@@ -57,6 +66,33 @@ public class InitialBuildProvider {
             InitialBuildFinished = true;
 //            FileUtils.appendTextToFile("log.txt", "\n updateInitialBuild end ==>> " + InitialBuildFinished);
         }
+        
+        if(InitialBuildFinished == false) {
+        	
+        	System.out.println("nowMarine ==>> " + nowMarine + " / orderMarine ==>> " + orderMarine);
+        	
+        	BuildOrderQueue iq = BuildManager.Instance().buildQueue;
+        	
+        	nowMarine = Prebot.Broodwar.self().completedUnitCount(UnitType.Terran_Marine);
+        	
+        	
+//        	20180721. hkk
+//        	마린을 프레임돌때마다 큐에 넣는것이 아니고. 생산이 될때마다 큐에 추가.
+//        	현재마린(nowMarine) 과 생산명령을 마린 숫자(orderMarine)를 비교
+//        	빌드큐에 넣으면 orderMarine++ 을 하고 현재 마린 숫자가 그것과 같아지면 다시 큐에 넣는다.
+        	if(nowMarine == orderMarine) {
+        	
+		        if (BuildManager.Instance().buildQueue.getItemCount(UnitType.Terran_Marine, null)
+						+ ConstructionManager.Instance().getConstructionQueueItemCount(UnitType.Terran_Marine, null) 
+						+ nowMarine < StrategyIdea.marineCount
+		        		&& Prebot.Broodwar.self().completedUnitCount(UnitType.Terran_Barracks) > 0) {
+		        	
+		        		iq.queueAsHighestPriority(UnitType.Terran_Marine, false);
+		        		orderMarine++;
+				}
+        	}
+        }
+	        
 
 
         //TODO 여기에 조건별 다 때려 넣으소서.......... 몇개 안되는거 굳이 나눌 필요가 있나싶다.
