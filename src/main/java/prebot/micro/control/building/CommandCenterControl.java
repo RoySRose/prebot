e7b056fe11d482b2cfcd305fe8d801f6561c37e5
Roy Kim <sungwook0115.kim@gmail.com>
2018-07-29 20:55:56 +0900

    Reconstruct Building Fly

--- src/main/java/prebot/micro/control/building/CommandCenterControl.java
+++ src/main/java/prebot/micro/control/building/CommandCenterControl.java
@@ -1,9 +1,5 @@
 package prebot.micro.control.building;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import bwapi.TilePosition;
 import bwapi.Unit;
 import bwapi.UnitType;
@@ -13,51 +9,67 @@ import prebot.common.main.Prebot;
 import prebot.common.util.UnitUtils;
 import prebot.micro.control.BuildingFly;
 import prebot.micro.control.BuildingFlyControl;
+import prebot.micro.control.FlyCondition;
 import prebot.strategy.InformationManager;
 import prebot.strategy.UnitInfo;
+
+import java.util.Collection;
 
 public class CommandCenterControl extends BuildingFlyControl {
 
 	@Override
 	public void control(Collection<Unit> unitList, Collection<UnitInfo> euiList) {
+
 		// TODO Auto-generated method stub
-        if (Prebot.Broodwar.self().completedUnitCount(UnitType.Terran_Command_Center) == 2
-                && Prebot.Broodwar.self().allUnitCount(UnitType.Terran_Command_Center) == 2) {
-            List<Unit> unitTempList = new ArrayList<>();
-            unitTempList.add(getSecondCommandCenter());
-            processFly(unitTempList, euiList);
+//        if (Prebot.Broodwar.self().completedUnitCount(UnitType.Terran_Command_Center) == 2
+//                && Prebot.Broodwar.self().allUnitCount(UnitType.Terran_Command_Center) == 2) {
+//            List<Unit> unitTempList = new ArrayList<>();
+//            unitTempList.add(getSecondCommandCenter());
+//            processFly(unitTempList, euiList);
+//        }
+
+        for(Unit unit :  unitList){
+
+            buildingFlyMap.put(unit, new FlyCondition());
+
+            if(unit.equals(InformationManager.Instance().getFirstCC())){
+                continue;
+            }
+            if(unit.equals(getSecondCommandCenter())) {
+                processFly(unit);
+            }
         }
 	}
 
     @Override
-    public void checkFlyCondition() {
+    public void checkFlyCondition(Unit checkCC) {
 
-        if (Prebot.Broodwar.self().completedUnitCount(UnitType.Terran_Command_Center) == 2) {
+        if (checkCC != null) {
+            BaseLocation correctLocation = InformationManager.Instance().getFirstExpansionLocation(InformationManager.Instance().selfPlayer);
 
-            Unit checkCC = getSecondCommandCenter();
-
-            if (checkCC != null) {
-                BaseLocation correctLocation = InformationManager.Instance().getFirstExpansionLocation(InformationManager.Instance().selfPlayer);
-
-                if (checkCC.isLifted() == false) {
-                    if (checkCC.getTilePosition().getX() != correctLocation.getTilePosition().getX() || checkCC.getTilePosition().getY() != correctLocation.getTilePosition().getY()) {
-                        setBuildingFly(BuildingFly.UP);
-                    }
-                } else {
-                    setLandPosition(new TilePosition(correctLocation.getTilePosition().getX(), correctLocation.getTilePosition().getY()));
-                    setBuildingFly(BuildingFly.DOWN);
+            if (checkCC.isLifted() == false) {
+                if (checkCC.getTilePosition().getX() != correctLocation.getTilePosition().getX() || checkCC.getTilePosition().getY() != correctLocation.getTilePosition().getY()) {
+                    buildingFlyMap.get(checkCC).setBuildingFly(BuildingFly.UP);
                 }
+            } else {
+                buildingFlyMap.get(checkCC).setLandPosition(new TilePosition(correctLocation.getTilePosition().getX(), correctLocation.getTilePosition().getY()));
+                buildingFlyMap.get(checkCC).setBuildingFly(BuildingFly.DOWN);
             }
         }
+
     }
 
     public Unit getSecondCommandCenter(){
 
-        for (Unit commandCenter : UnitUtils.getUnitList(UnitType.Terran_Command_Center)) {
-            if (commandCenter.getTilePosition().getX() == BlockingEntrance.Instance().starting.getX() && commandCenter.getTilePosition().getY() == BlockingEntrance.Instance().starting.getY()) {
-                continue;
-            } else {
-                return commandCenter;
+        if (Prebot.Broodwar.self().completedUnitCount(UnitType.Terran_Command_Center) == 2
+                && Prebot.Broodwar.self().allUnitCount(UnitType.Terran_Command_Center) == 2) {
+
+            for (Unit commandCenter : UnitUtils.getUnitList(UnitType.Terran_Command_Center)) {
+                if (commandCenter.getTilePosition().getX() == BlockingEntrance.Instance().starting.getX() && commandCenter.getTilePosition().getY() == BlockingEntrance.Instance().starting.getY()) {
+                    continue;
+                } else {
+                    return commandCenter;
+                }
             }
         }
         return null;
--- src/main/java/prebot/micro/control/building/EngineeringBayControl.java
+++ src/main/java/prebot/micro/control/building/EngineeringBayControl.java
@@ -3,23 +3,26 @@ package prebot.micro.control.building;
 import java.util.Collection;
 
 import bwapi.Unit;
+import prebot.build.initialProvider.BlockingEntrance.BlockingEntrance;
 import prebot.micro.control.BuildingFlyControl;
+import prebot.micro.control.FlyCondition;
+import prebot.strategy.InformationManager;
 import prebot.strategy.UnitInfo;
 
 public class EngineeringBayControl extends BuildingFlyControl {
 
-    public EngineeringBayControl() {
-        super(true, false, null);
-    }
-
-	@Override
+ 	@Override
 	public void control(Collection<Unit> unitList, Collection<UnitInfo> euiList) {
-  
-        processFly(unitList, euiList);
+
+        for(Unit unit :  unitList){
+
+            buildingFlyMap.put(unit, new FlyCondition(true, false, null));
+            processFly(unit);
+        }
 	}
 
     @Override
-    public void checkFlyCondition() {
+    public void checkFlyCondition(Unit unit) {
 
     }
 }
