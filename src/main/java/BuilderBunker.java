61e81ff5156d5abbee260d17eb7c619d04452e22
DESKTOP-549JLBN\jw <jerrioh@naver.com>
2018-08-24 23:52:38 +0900

    벙커

--- src/main/java/BuilderBunker.java
+++ src/main/java/BuilderBunker.java
@@ -68,8 +68,8 @@ public class BuilderBunker extends DefaultBuildableItem {
 			if (bunkerBuildLeftSecond == 0) {
 				setBlocking(true);
 				setHighPriority(true);
-//				setTilePosition(BlockingEntrance.Instance().bunker1);
-				setTilePosition(BlockingEntrance.Instance().first_supple);
+				setTilePosition(BlockingEntrance.Instance().bunker1);
+//				setTilePosition(BlockingEntrance.Instance().first_supple);
 				return true;
 			}
 			
